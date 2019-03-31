39bf18297d0d0e1f526e664044806f46b9071d18
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-03-31 20:34:12 +1300

    night-patch photo set

--- ClassDemo/FlashChat/app/src/main/java/com/HirciTech/YemaoChat/ChatListAdapter.java
+++ ClassDemo/FlashChat/app/src/main/java/com/HirciTech/YemaoChat/ChatListAdapter.java
@@ -2,8 +2,10 @@ package com.HirciTech.YemaoChat;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -108,11 +110,28 @@ public class ChatListAdapter extends BaseAdapter {
         final  InstandMessage message = getItem(i);
         final viewHolder holder = (viewHolder)view.getTag();
         holder.authorName.setText(message.getAuthor());
+        boolean isAuthor = message.getAuthor().equals(mDisplayName);
         holder.body.setText(message.getMessage());
+        this.setChatRowApperance(isAuthor,holder);
         return view;
     }
     public void cleanup()
     {
         this.mDatabaseReference.removeEventListener(mChildEventListener);
+    }
+
+    private void setChatRowApperance(boolean isAuthor, viewHolder holder){
+        if(isAuthor){
+            holder.Params.gravity = Gravity.END;
+            holder.authorName.setTextColor(Color.RED);
+            holder.body.setBackgroundResource(R.drawable.bubble2);
+        }
+        else{
+            holder.Params.gravity = Gravity.START;
+            holder.authorName.setTextColor(Color.BLUE);
+            holder.body.setBackgroundResource(R.drawable.bubble1);
+        }
+        holder.authorName.setLayoutParams(holder.Params);
+        holder.body.setLayoutParams(holder.Params);
     }
 }
